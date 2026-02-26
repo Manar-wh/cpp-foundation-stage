3e310784d4371f657344fc752c59c5ba854cbc2a
Manar Alwahedi <alwmanar@gmail.com>
2026-02-26 06:57:15 +0300

    add 12x12 multiplication table using 2D arrays

---
+++ 06-cpp-level-2/01-two-dimensional-arrays/01-multiplication-table-2d-array.cpp
@@ -0,0 +1,40 @@
+/*
+  Course: C++ Level 2 (Course 06)
+  Topic: 2D Arrays
+  Project: 12x12 Multiplication Table
+  Description: Fills a 2D array with multiplication results and prints it formatted.
+*/
+
+#include <iostream>
+#include <cstdio> // Required for printf
+
+using namespace std;
+
+// Function to fill the 2D array with multiplication table data
+void fillarr(int Mult[12][12]) {
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            Mult[i][j] = (i + 1) * (j + 1);
+        }
+    }
+}
+
+// Function to print the 2D array with proper formatting
+void printarr(int Mult[12][12]) {
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            // Using printf for aligned columns
+            printf("%*d ", 3, Mult[i][j]);
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int Mult[12][12];
+
+    fillarr(Mult);
+    printarr(Mult);
+
+    return 0;
+}
