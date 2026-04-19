f79bf48cf647e1634029211b5e09e1b47f7a5fa2
Manar Alwahedi <alwmanar@gmail.com>
2026-04-19 08:39:17 +0300

    13-check-identity-matrix.cpp

--- 07-algorithms-level-3/13-check-identity-matrix.cpp
+++ 07-algorithms-level-3/13-check-identity-matrix.cpp
@@ -4,63 +4,51 @@
 
 using namespace std;
 
-void PrintMatrix(int Matrix[3][3], short Row, short Cols) {
-
-	for (int i = 0; i < Row; i++)
-	{
-		for (int j = 0; j < Cols; j++)
-		{
-			printf("%02d \t", Matrix[i][j]);
-		}
-		cout << "\n";
-	}
+void PrintMatrix(int Matrix[3][3], short Rows, short Cols) {
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            printf("%02d \t", Matrix[i][j]);
+        }
+        cout << "\n";
+    }
 }
 
-bool CheckMatricesIdentity(int Matrix[3][3],short Row, short Cols) {
-
-
-	for (int i = 0; i < Row; i++)
-	{
-		for (int j = 0; j < Cols; j++)
-		{
-			
-			if (i == j && Matrix[i][j] != 1)
-			{
-				return false;
-			}
-			else if (i != j && Matrix[i][j] != 0) {
-				return false;
-			}
-		}
-	}
-	return true;
+bool IsIdentityMatrix(int Matrix[3][3], short Rows, short Cols) {
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            if (i == j && Matrix[i][j] != 1) {
+                return false;
+            }
+            else if (i != j && Matrix[i][j] != 0) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
-void PrintMatricesAreTypical(int Matrix[3][3], short Row, short Cols) {
-
-	if (CheckMatricesIdentity(Matrix, Row, Cols))
-	{
-		cout << "\nYes: Matrices Are Identity\n";
-	}
-	else
-		cout << "\nNo: Matrices Are Not Identity\n";
+void DisplayIdentityResult(int Matrix[3][3], short Rows, short Cols) {
+    if (IsIdentityMatrix(Matrix, Rows, Cols)) {
+        cout << "\nYES: Matrix is Identity.\n";
+    }
+    else {
+        cout << "\nNO: Matrix is NOT Identity.\n";
+    }
 }
 
 int main() {
+    srand((unsigned)time(NULL));
 
-	srand((unsigned)time(NULL));
+    int Matrix[3][3] = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 1}
+    };
 
-	int Matrix[3][3] = {
-		{1,0,0},
-		{0,1,0},
-		{0,0,1}
-	};
+    cout << "Matrix: \n";
+    PrintMatrix(Matrix, 3, 3);
 
-	cout << "Matrix: \n";
+    DisplayIdentityResult(Matrix, 3, 3);
 
-	PrintMatrix(Matrix, 3, 3);
-
-	
-	PrintMatricesAreTypical(Matrix, 3, 3);
-
+    return 0;
 }
