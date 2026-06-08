93081251403b25c06e6782522c27fd92f2b7ac47
Manar Alwahedi <alwmanar@gmail.com>
2026-06-08 12:03:04 +0300

    Implement HowManyDaysBetweenTwoDates using AddOneDayToDate

---
+++ 08-algorithms-level-4/18-how-many-days-between-two-dates.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+short ReadYear() {
+	short Year;
+	cout << "Please enter The Year ? ";
+	cin >> Year;
+	return Year;
+}
+
+short ReadMonth() {
+	short Month;
+	cout << "Please enter The Month ? ";
+	cin >> Month;
+	return Month;
+}
+
+short ReadDay() {
+	short Day;
+	cout << "Please enter The Day ? ";
+	cin >> Day;
+	return Day;
+}
+
+short IsLeapYear(short Year) {
+	return (Year % 400 == 0) || (Year % 4 == 0 && Year % 100 != 0);
+}
+
+short NumberOfDaysInMonth(short Month, short Year) {
+	if (Month < 1 || Month > 12)
+	{
+		return 0;
+	}
+	short arrDaysInMonth[12] = { 31 , 28, 31, 30, 31 , 30 , 31 , 31, 30, 31 , 30, 31 };
+	return (Month == 2) ? (IsLeapYear(Year) ? 29 : 28) : (arrDaysInMonth[Month - 1]);
+}
+
+struct stDate {
+	short Day;
+	short Month;
+	short Year;
+};
+
+stDate ReadFullDate() {
+	stDate Date;
+	Date.Day = ReadDay();
+	Date.Month = ReadMonth();
+	Date.Year = ReadYear();
+	return Date;
+}
+
+bool IsLastDayInMonth(stDate Date) {
+	return (Date.Day == NumberOfDaysInMonth(Date.Month, Date.Year));
+}
+
+bool IsLastMonthInYear(stDate Date) {
+	return (Date.Month == 12);
+}
+
+bool IsDate1EqualDate2(stDate Date1, stDate Date2) {
+	return (Date1.Year == Date2.Year) ?
+		((Date1.Month == Date2.Month) ? (Date1.Day == Date2.Day) : false)
+		: false;
+}
+
+bool IsDate1BeforeDate2(stDate Date1, stDate Date2) {
+	return (Date1.Year < Date2.Year) ? true :
+		(Date1.Year == Date2.Year) ?
+		((Date1.Month < Date2.Month) ? true :
+			(Date1.Month == Date2.Month) ? (Date1.Day < Date2.Day) : false) : false;
+}
+
+stDate AddOneDayToDate(stDate Date) {
+	if (IsLastDayInMonth(Date))
+	{
+		if (IsLastMonthInYear(Date))
+		{
+			Date.Day = 1;
+			Date.Month = 1;
+			Date.Year++;
+		}
+		else
+		{
+			Date.Day = 1;
+			Date.Month++;
+		}
+	}
+	else
+	{
+		Date.Day++;
+	}
+	return Date;
+}
+
+int HowManyDaysBetweenTwoDates(stDate Date1, stDate Date2, bool IncludeEndDay = false) {
+	int Days = 0;
+	while (IsDate1BeforeDate2(Date1, Date2))
+	{
+		Date1 = AddOneDayToDate(Date1);
+		Days++;
+	}
+	return (IncludeEndDay) ? ++Days : Days;
+}
+
+int main() {
+	stDate Date1 = ReadFullDate();
+	cout << "\n";
+	stDate Date2 = ReadFullDate();
+
+	cout << "\nDiffrence is: " << HowManyDaysBetweenTwoDates(Date1, Date2) << " Day(s)\n";
+	cout << "\nDiffrence (Includeng end day) is: " << HowManyDaysBetweenTwoDates(Date1, Date2, true) << " Day(s)\n";
+
+	system("pause > 0");
+	return 0;
+}
