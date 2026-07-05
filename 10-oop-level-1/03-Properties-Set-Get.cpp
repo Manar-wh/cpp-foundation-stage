e142472e8944c3b28f5c27dd5b51931e0c51d5fa
Manar Alwahedi <alwmanar@gmail.com>
2026-07-05 06:29:11 +0300

    feat: implement encapsulated class properties using modern set and get methods

---
+++ 10-oop-level-1/03-Properties-Set-Get.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class clsPerson {
+
+    string _FirstName;
+    string _LastName;
+
+public:
+
+    void setFirstName(string FirstName) {
+
+        _FirstName = FirstName;
+    }
+
+    string FirstName() {
+        return _FirstName;
+    }
+
+    void setLastName(string LastName) {
+        _LastName = LastName;
+    }
+
+    string LastName() {
+        return _LastName;
+    }
+};
+
+int main()
+{
+    clsPerson Person1;
+
+    Person1.setFirstName("Manar");
+    Person1.setLastName("Alwahedi");
+    cout << Person1.FirstName() << " " << Person1.LastName();
+
+}
