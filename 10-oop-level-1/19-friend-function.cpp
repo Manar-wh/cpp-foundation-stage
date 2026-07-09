4824fe2fe7cc4f68f2d76215fd4845edb1d91f68
Manar Alwahedi <alwmanar@gmail.com>
2026-07-09 10:20:18 +0300

    19-friend-function

---
+++ 10-oop-level-1/19-friend-function.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class clsA {
+
+	int _Var1;
+
+protected:
+
+	int Var2;
+
+public:
+	int Var3;
+
+	clsA()
+	{
+		_Var1 = 9;
+		Var2 = 10;
+		Var3 = 5;
+	}
+	
+	friend void Display(clsA A);
+};
+
+void Display(clsA A) {
+	
+	cout << "The value of Var1 = " << A._Var1 << "\n";
+	cout << "The value of Var2 = " << A.Var2 << "\n";
+	cout << "The value of Var3 = " << A.Var3 << "\n";
+}
+
+
+int main() {
+
+	clsA A;
+	Display(A);
+	return 0;
+}
