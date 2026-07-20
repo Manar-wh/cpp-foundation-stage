ff7fa078b8afcef7e90f48e78a959370905c5f32
Manar Alwahedi <alwmanar@gmail.com>
2026-07-20 23:30:06 +0300

    Main.cpp

---
+++ 11- oop-applications/04-project3-bank-main-menu/04-delete-client-screen/Main.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+#include "clsMainScreen.h"
+#include "clsScreen.h"
+
+int main()
+{
+	clsMainScreen::ShowMainMenuScreen();
+
+}
