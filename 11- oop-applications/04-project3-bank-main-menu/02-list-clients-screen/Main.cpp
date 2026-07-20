10a7825a99fd7909e324fd417fbe535e003d753c
Manar Alwahedi <alwmanar@gmail.com>
2026-07-20 11:11:02 +0300

    Main.cpp

--- 11- oop-applications/04-project3-bank-main-menu/02-list-clients-screen/Main.cpp
+++ 11- oop-applications/04-project3-bank-main-menu/02-list-clients-screen/Main.cpp
@@ -1,1 +1,10 @@
-.
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+#include "clsMainScreen.h"
+#include "clsScreen.h"
+
+int main()
+{
+	clsMainScreen::ShowMainMenuScreen();
+}
