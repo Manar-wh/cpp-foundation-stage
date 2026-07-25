a02795528f8697576f7c954bfea9969e028e9293
Manar Alwahedi <alwmanar@gmail.com>
2026-07-26 00:24:30 +0300

    clsScreen.h

---
+++ 11- oop-applications/07-project3-bank-login-permissions/04-apply-permissions/clsScreen.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include "clsGlobal.h"
+#include "clsUser.h"
+using namespace std;
+class clsScreen
+{
+protected:
+	static void _DrawScreenHeader(string Title, string SubTitle = "")
+	{
+		cout << "\t\t\t\t\t______________________________________";
+		cout << "\n\n\t\t\t\t\t  " << Title;
+		if (SubTitle != "")
+		{
+			cout << "\n\t\t\t\t\t  " << SubTitle;
+		}
+		cout << "\n\t\t\t\t\t______________________________________\n\n";
+	}
+
+	static bool CheckAccessRights(clsUser::enPermissions Permissions) {
+
+		if (!CurrentUser.CheckAccessPermission(Permissions))
+		{
+			cout << "\t\t\t\t\t-----------------------------------";
+			cout << "\n\t\t\t\t\tAccess Denied! Contact your Admin.";
+			cout << "\n\t\t\t\t\t-----------------------------------\n";
+			return false;
+		}
+		return true;
+	}
+};
+
+
