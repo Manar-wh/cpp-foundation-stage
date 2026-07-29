6992bb733bcbc6a56b7042984b8369e97d7ac673
Manar Alwahedi <alwmanar@gmail.com>
2026-07-29 12:11:17 +0300

    clsTransactionsScreen.h

---
+++ 11- oop-applications/15-bank-extension-08-show-transfer-log-screen/clsTransactionsScreen.h
@@ -0,0 +1,123 @@
+#pragma once
+#include <iostream>
+using namespace std;
+#include <iomanip>
+#include "clsInputValidate.h"
+#include "clsScreen.h"
+#include "clsDeposit.h"
+#include "clsWithdraw.h"
+#include "clsBalancesListScreen.h"
+#include "clsUser.h"
+#include "clsTransferScreen.h"
+#include "clsTransferLogScreen.h"
+
+class clsTransactionsScreen : protected clsScreen
+{
+	enum enTransactions 
+	{
+		DepositOp = 1, WithdrawOp = 2, TotalBalancesOp = 3, TransferOp = 4 , TransferLogOp = 5, MainMenuOp = 6
+	};
+
+	static short _ReadTransactionsOption() {
+		cout << setw(37) << left << "" << "Please Choose What You Want To Do? [1 to 6]:  ";
+		short Option = 0;
+		Option = clsInputValidate::ReadShortNumberBetween(1, 6);
+		return Option;
+	}
+
+	static void _performeMainMenuoptions(enTransactions Option) {
+
+		switch (Option)
+		{
+		case clsTransactionsScreen::DepositOp:
+			system("cls");
+			_ShowDepositScreen();
+			_GoBackToTransactionsMenu();
+			break;
+
+		case clsTransactionsScreen::WithdrawOp:
+			system("cls");
+			_ShowWithdrawScreen();
+			_GoBackToTransactionsMenu();
+			break;
+
+		case clsTransactionsScreen::TotalBalancesOp:
+			system("cls");
+			_ShowTotalBalancesScreen();
+			_GoBackToTransactionsMenu();
+			break;
+
+		case clsTransactionsScreen::TransferOp:
+			system("cls");
+			_ShowTransferScreen();
+			_GoBackToTransactionsMenu();
+			break;
+
+		case clsTransactionsScreen::TransferLogOp:
+			system("cls");
+			_ShowTransferLogScreen();
+			_GoBackToTransactionsMenu();
+			break;
+
+		case clsTransactionsScreen::MainMenuOp:
+			system("cls");
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	static void _ShowDepositScreen() {
+		clsDeposit::ShowDepositScreen();
+	}
+
+	static void _ShowWithdrawScreen() {
+		clsWithdraw::ShowWithdrawScreen();
+	}
+
+	static void _ShowTotalBalancesScreen() {
+		clsBalancesListScreen::ShowBalancesListScreen();
+	}
+
+	static void _ShowTransferScreen() {
+		clsTransferScreen::ShowTransferScreen();
+	}
+
+	static void _ShowTransferLogScreen() {
+		clsTransferLogScreen::ShowTransferLogScreen();
+	}
+
+	static void _GoBackToTransactionsMenu() {
+		system("pause");
+		system("cls");
+		ShowTransactionsMenuScreen();
+	}
+
+public:
+
+	static void ShowTransactionsMenuScreen() {
+
+		if (!_CheckAccessRights(clsUser::pTransactions))
+		{
+			return;
+		}
+
+		system("cls");
+		_DrawScreenHeader("\t Transactions Menu Screen");
+
+		cout << setw(37) << left << "" << "==============================================\n";
+		cout << setw(37) << left << "" << "\t\t Transactions Menu Screen\n";
+		cout << setw(37) << left << "" << "==============================================\n";
+		cout << setw(37) << left << "" << "\t[1] Deposit\n";
+		cout << setw(37) << left << "" << "\t[2] Withdraw \n";
+		cout << setw(37) << left << "" << "\t[3] Total Balances \n";
+		cout << setw(37) << left << "" << "\t[4] Transfer \n";
+		cout << setw(37) << left << "" << "\t[5] Transfer Log\n";
+		cout << setw(37) << left << "" << "\t[6] Main Menu\n";
+		cout << setw(37) << left << "" << "==============================================\n";
+		_performeMainMenuoptions((enTransactions)_ReadTransactionsOption());
+	}
+
+};
+
