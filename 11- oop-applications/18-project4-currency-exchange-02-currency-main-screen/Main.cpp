b71153f172e9e7c7fb8808c1bed71cc19ef77a28
Manar Alwahedi <alwmanar@gmail.com>
2026-08-01 23:50:37 +0300

    Main.cpp

---
+++ 11- oop-applications/18-project4-currency-exchange-02-currency-main-screen/Main.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "clsLoginScreen.h"
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		if (!clsLoginScreen::ShowLoginScreen())
+		{
+			break;
+		}
+	}
+
+}
