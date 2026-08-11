c80de67eaeb1dc6b2c644020c84a4b4651991439
Manar Alwahedi <alwmanar@gmail.com>
2026-08-11 09:15:51 +0300

    Implement DeleteFirstNode operation in singly linked list

---
+++ 12-data-structures-level1/10-singly-linked-list-delete-first-node.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int Value;
+	Node* Next;
+};
+
+void PrintList(Node* head) {
+
+	while (head != NULL)
+	{
+		cout << head->Value << "  ";
+		head = head->Next;
+	}
+	cout << "\n";
+}
+
+void InsertAtEnd(Node* &head, int value) {
+
+	Node* new_node = new Node();
+	new_node->Value = value;
+	new_node->Next = NULL;
+	
+	if (head == NULL)
+	{
+		head = new_node;
+		return;
+	}
+
+	Node* LastNode = head;
+
+	while (LastNode->Next != NULL)
+	{
+		LastNode = LastNode->Next;
+	}
+
+	LastNode->Next = new_node;
+	return;
+}
+
+void DeleteFirstNode(Node* &head) {
+	if (head == NULL) return;
+	Node* FirstNode = head; 
+	head = head->Next;
+	delete FirstNode;
+}
+
+int main()
+{
+	Node* head = NULL;
+	InsertAtEnd(head, 1);
+	InsertAtEnd(head, 2);
+	InsertAtEnd(head, 3);
+	InsertAtEnd(head, 4);
+	InsertAtEnd(head, 5);
+
+	PrintList(head);
+	
+	DeleteFirstNode(head);
+	PrintList(head);
+}
