c04d1df7f48de35f1bf285c3f5fbc3c2537ec65a
Manar Alwahedi <alwmanar@gmail.com>
2026-08-11 15:35:28 +0300

    Implement InsertAfter operation in doubly linked list

---
+++ 12-data-structures-level1/14-doubly-linked-list-insert-after.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int Value;
+	Node* Next;
+	Node* Prev;
+};
+
+void InsertAtBeginning(Node* &head, int value) {
+	
+	Node* new_node = new Node();
+	new_node->Value = value;
+	new_node->Next = head;
+	new_node->Prev = NULL;
+
+	if (head != NULL)
+	{
+		head->Prev = new_node;
+	}
+
+	head = new_node;
+}
+
+void PrintList(Node* head) {
+	while (head != NULL)
+	{
+		cout << head->Value << " ";
+		head = head->Next;
+	}
+	cout << "\n";
+}
+
+void InsertAfter(Node*  Current, int value) {
+
+	if (Current == NULL) return;
+
+	Node* new_node = new Node();
+	new_node->Value = value;
+
+	new_node->Prev = Current;
+	new_node->Next = Current->Next;
+
+	if (Current->Next != NULL)
+	{
+		Current->Next->Prev = new_node;
+	}
+	Current->Next = new_node;
+}
+
+int main()
+{
+	Node* head = NULL;
+
+	InsertAtBeginning(head, 1);
+	InsertAtBeginning(head, 2);
+	InsertAtBeginning(head, 3);
+	InsertAtBeginning(head, 4);
+
+	InsertAfter(head, 500);
+	PrintList(head);
+
+}
