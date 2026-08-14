ec7f9b1d0c23275c992192c127900836f1fdd5a6
Manar Alwahedi <alwmanar@gmail.com>
2026-08-14 14:45:09 +0300

    Create Main.cpp

---
+++ 13-algorithms-level-5/01-project-1/02-extension-01-add-size-method/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include "clsDblLinkedList.h"
+int main()
+{
+	clsDblLinkedList <int> MydblLinkedList;
+	MydblLinkedList.InsertAtBeginning(5);
+	MydblLinkedList.InsertAtBeginning(4);
+	MydblLinkedList.InsertAtBeginning(3);
+	MydblLinkedList.InsertAtBeginning(2);
+	MydblLinkedList.InsertAtBeginning(1);
+
+	cout << "Link List Content: \n";
+	MydblLinkedList.PrintList();
+
+	 cout << "\nNumber of the Item in Linked List: " << MydblLinkedList.Size() << "\n";
+
+}
