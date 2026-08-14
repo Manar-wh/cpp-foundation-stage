dbd0df22d8e745f6bc85389c535fa618a29344f0
Manar Alwahedi <alwmanar@gmail.com>
2026-08-14 21:22:48 +0300

    Main.cpp

---
+++ 13-algorithms-level-5/01-project-1/05-extension-04-add-reverse-method/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "clsDblLinkedList.h"
+using namespace std;
+
+int main()
+{
+    clsDblLinkedList <int> MydblLinkedList;
+
+    MydblLinkedList.InsertAtBeginning(5);
+    MydblLinkedList.InsertAtBeginning(4);
+    MydblLinkedList.InsertAtBeginning(3);
+    MydblLinkedList.InsertAtBeginning(2);
+    MydblLinkedList.InsertAtBeginning(1);
+
+    cout << "\nLinked List Contenet:\n";
+    MydblLinkedList.PrintList();
+
+   MydblLinkedList.Reverse();
+   cout << "\nLinked List Content After Reverse: \n";
+   MydblLinkedList.PrintList();
+
+
+    system("pause>0");
+}
