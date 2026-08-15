8e9b9ad0bd69072df4d6944cfcdc7bd9386df5d7
Manar Alwahedi <alwmanar@gmail.com>
2026-08-15 07:43:40 +0300

    Main.cpp

---
+++ 13-algorithms-level-5/01-project-1/06-extension-05-get-node/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "clsDblLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+    clsDblLinkedList <int> MydblLinkedList;
+
+    MydblLinkedList.InsertAtBeginning(5);
+    MydblLinkedList.InsertAtBeginning(4);
+    MydblLinkedList.InsertAtBeginning(3);
+    MydblLinkedList.InsertAtBeginning(2);
+    MydblLinkedList.InsertAtBeginning(1);
+
+    cout << "\nLinked List Contenet:\n";
+    MydblLinkedList.PrintList();
+
+    clsDblLinkedList <int>::Node* N;
+
+    N = MydblLinkedList.GetNode(2);
+    cout << "\nNode Value Is: " << N->Value << "\n";
+
+    system("pause>0");
+}
