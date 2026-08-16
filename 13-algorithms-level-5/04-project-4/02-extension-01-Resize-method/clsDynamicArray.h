477a47becaf783c9c83faa73c8f0e4ca9dac5c80
Manar Alwahedi <alwmanar@gmail.com>
2026-08-16 16:47:07 +0300

    clsDynamicArray.h

---
+++ 13-algorithms-level-5/04-project-4/02-extension-01-Resize-method/clsDynamicArray.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class clsDynamicArray
+{
+	int _Size = 0;
+	T* _TempArray;
+
+public:
+
+	T* OriginalArray;
+
+	clsDynamicArray(int Size = 0) {
+		if (Size < 0)
+			Size = 0;
+
+		_Size = Size;
+		OriginalArray = new T[Size];
+	}
+
+	bool SetItem(int Index, T Value) {
+
+		if (Index < 0 || Index >= _Size)
+			return false;
+
+		OriginalArray[Index] = Value;
+		return true;
+	}
+
+	void PrintList() {
+
+		for (int i = 0; i < _Size; i++)
+		{
+			cout << OriginalArray[i] << "  ";
+		}
+	}
+
+	bool IsEmpty() {
+		return (_Size == 0);
+	}
+
+	int ArraySize() {
+		return _Size;
+	}
+
+	void Resize(int NewSize = 0) {
+
+		if (NewSize < 0)
+			NewSize = 0;
+		_TempArray = new T[NewSize];
+
+		if (NewSize < _Size)
+			_Size = NewSize;
+
+		for (int i = 0; i < _Size; i++)
+		{
+			_TempArray[i] = OriginalArray[i];
+		}
+
+		delete[]OriginalArray;
+		_Size = NewSize;
+		OriginalArray = _TempArray;
+
+	}
+
+	~clsDynamicArray() {
+		delete[] OriginalArray; 
+	}
+
+};
+
