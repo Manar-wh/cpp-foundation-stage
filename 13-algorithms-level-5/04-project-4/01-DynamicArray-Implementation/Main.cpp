cd23c5a299a156020e83c396b2afe78ce5870820
Manar Alwahedi <alwmanar@gmail.com>
2026-08-16 15:31:43 +0300

    Main.cpp

---
+++ 13-algorithms-level-5/04-project-4/01-DynamicArray-Implementation/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "clsDynamicArray.h"
+
+using namespace std;
+
+int main()
+{
+    clsDynamicArray <int> MyDynamicArray(5);
+
+    MyDynamicArray.SetItem(0,1);
+    MyDynamicArray.SetItem(1,2);
+    MyDynamicArray.SetItem(2,3);
+    MyDynamicArray.SetItem(3,4);
+    MyDynamicArray.SetItem(4,5);
+
+    cout << "\nIs Empty   ?  " << MyDynamicArray.IsEmpty();
+    cout << "\nArray Size :  " << MyDynamicArray.ArraySize();
+    cout << "\nArray Item :  ";
+    MyDynamicArray.PrintList();
+
+
+    system("pause>0");
+    return 0;
+}
