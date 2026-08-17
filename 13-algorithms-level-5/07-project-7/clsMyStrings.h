d9026987094e0f0ba6023b7361bd1cd8bc01e7d5
Manar Alwahedi <alwmanar@gmail.com>
2026-08-17 15:48:23 +0300

    Implement clsMyString with Undo/Redo operations

---
+++ 13-algorithms-level-5/07-project-7/clsMyStrings.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class clsMyStrings 
+{
+	string _Value;
+public:
+
+	stack <string> _Undo;
+	stack <string> _Redo;
+
+	void SetValue(string Value) {
+		_Undo.push(_Value);
+		_Value = Value;
+
+		while (!_Redo.empty()) {
+			_Redo.pop();
+		}
+	}
+
+	string GetValue() {
+		return _Value;
+	}
+
+	__declspec(property(get = GetValue, put = SetValue)) string Value;
+
+	void Undo() {
+
+		if (!_Undo.empty()) {
+			_Redo.push(_Value);
+			_Value = _Undo.top();
+			_Undo.pop();
+		}
+	}
+
+	void Redo() {
+
+		if (!_Redo.empty()) {
+
+			_Undo.push(_Value);
+			_Value = _Redo.top();
+			_Redo.pop();
+		}
+	}
+};
+
